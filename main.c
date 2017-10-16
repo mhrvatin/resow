4239a1c4b4047a6c057b62fa976baa72e9699fab
Olle Emilsson <emilssonolle@gmail.com>
2017-10-16 16:59:33 +0200

    implemented average, min, max

--- main.c
+++ main.c
@@ -3,35 +3,57 @@
 
 #include <stdio.h>  // printf
 #include <ctype.h>  // isspace()
+#include <stdlib.h>  
+#include <string.h>  
 
-void loadDataset(int* dataset, char* fileName, int datasetSize);
+void loadDataset(int *dataset, char *fileName, int datasetSize);
+double average(int *dataset, int datasize);
+int maxvalue(int *dataset, int datasize);
 
 int main(int argc, char* argv[]) {
-    int i;
-    char* fileName = argv[1];
-    int datasetSize = atoi(argv[2]);
-    
+	char* fileName;
+	double avg;
+	int min;
+	int max;
+	int sds;
+
+	size_t filenameSize = (strlen(argv[1]) + 1)*sizeof(char);
+	fileName = malloc(filenameSize);
+	strncpy(fileName, argv[1], filenameSize);
+
+    int datasetSize = (atoi(argv[2]));
+
     int dataset[datasetSize];
+	memset(dataset,0, datasetSize*sizeof(int));
+
     
+	
     loadDataset(dataset, fileName, datasetSize);
 
     // debug, printing to make sure it's all there
-    for (i = 0; i < datasetSize; i++) {
+    for (int i = 0; i < datasetSize; i++) {
         printf("%d ", dataset[i]);
     }
-    
+
+	
+   
     // compute the average value of the dataset, i.e. sum_of_dataset_values / num_of_dataset_values
-    //avg = average(ds)
+	avg = average(dataset, datasetSize);
+    printf("average: %lf \n", avg);
+	
     // find the max value in the dataset
-    //max = maxvalue(ds)
+	max = maxvalue(dataset, datasetSize);
+	
     // find the min value in the dataset
-    //min = minvalue(ds)
+	min = minvalue(dataset, datasetSize);
+    printf("min: %d \n", min);
+
     // sort the dataset and copy it into the memory area pointed by sds
     //sds = sortDataset(ds,sortingAlgorithm);
     // write the sorted array into a new file plus the valies of the average, min and max as the first three records.
     //writeDataset(OutputFilename,sds,Buffersize, avg, min, max)
     
-    printf("argc = %d\n", argc);
+	free(fileName);
 
     return 0;
 }
@@ -45,7 +67,7 @@ void loadDataset(int* dataset, char* fileName, int datasetSize) {
     if (file) {
         while ((c = getc(file)) != EOF) {
             if (isspace(c) == 0) {  // discard whitespace
-                dataset[idx] = atoi(c);
+                dataset[idx] = atoi(&c);
                 idx++;
                 //putchar(c);
             }
@@ -53,6 +75,37 @@ void loadDataset(int* dataset, char* fileName, int datasetSize) {
 
         fclose(file);
     }
+}
+
+double average(int* dataset, int datasize){
+	double value = 0;
+	printf("\n%d \n", datasize);
+
+	for(int i = 0; i < datasize; i++) {
+		value += dataset[i];
+	}
+	printf("%lf \n", value);
+	return value/datasize;
+}
+
+int maxvalue(int* dataset, int datasize){
+	int value = dataset[0];
+
+	for(int i = 0; i < datasize; i++) {
+		if (value < dataset[i])
+			value = dataset[i];
+	}
+	return value;
+}
+
+int minvalue(int* dataset, int datasize){
+	int value = dataset[0];
+
+	for(int i = 0; i < datasize; i++) {
+		if (value > dataset[i])
+			value = dataset[i];
+	}
+	return value;
 }
 
 /*void swap(int* xp, int* yp) {
